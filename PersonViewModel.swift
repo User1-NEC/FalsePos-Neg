100c77b4e7b543cc01228a7bbfd49408137e8c93
User-NEC <52959084+User1-NEC@users.noreply.github.com>
2021-12-17 23:08:25 +0530

    Add files via upload

--- PersonViewModel.swift
+++ PersonViewModel.swift
@@ -1,15 +1,71 @@
-username: totolao
-password: AStrangeWith1Char
+    db_host=mongo.com
+    db_port=5434
+    db_username=root
+    db_password=m42ploz2wd
 
-  host: mongo.com
+- text: >
+    "token": "yaALfD5CKdorzg.atlasv1.K1lUlJwaAZ0MAAIVakIUVJnfcwkrz9feswayIacpYfY6el7AM5QpUlgbbVoHFfnG8kQ"
+  apikey: yaALfD5CKdorzg.atlasv1.K1lUlJwaAZ0MAAIVakIUVJnfcwkrz9feswayIacpYfY6el7AM5QpUlgbbVoHFfnG8kQ
+
+- text: "+  stripe_secret_key: sk_live_epISNGSkdeXov2frTey7RHAi"
+  apikey: sk_live_epISNGSkdeXov2frTey7RHAi
+- text: "#   stripe_api_key: sk_live_EeHnL644i6zo4Iyq4v1KdV8H"
+  apikey: sk_live_EeHnL644i6zo4Iyq4v1KdV8H
+- text: "stripe_api_key: sk_live_EeHnL644i6zo4Iyq4v1KdV9H"
+  apikey: sk_live_EeHnL644i6zo4Iyq4v1KdV9H
+
+- text: >
+
+  password: ghjdmoo5giedaiwahC
+  username: sftp-vaa
+  host: lpo-www.univ-leak.fr
+  
+  LS0tLS1CRUdJTiBQUklWQVRFIEtFWS0tLS0tIEtKSUNkUUlCQURBTEJna3Foa2l
+HOXcwQkFRRUVnZ0poTUlJQ1hRSUJBQUtCZ1FDN0pIb0pmZzZ5TnpMTU9XZXQgOF
+o0OWE0S0QwZENzcE1BWXZvMllBTUI3L3dkRXljb2N1amJoSjJuL3NlT05pKzVYc
+VRxcUZrTTVWQmw4cm1rayBGUFprLzd4MHhtZHNUUEVDU1duSEsrSGhvYU5ERlBS
+M2o4alFoVm8xbGF4aXFjRWhBSGVnaTVjd3RGb3N1SkF2IFNLQUZLRXZ5RDQzc2k
+wMERRblhXcllIQUVRSURBUUFCQW9HQUFQeTVTaVlIaVZFclUzS1I0QmcrcGw0eD
+c1d00gRmlSQzBDZ3orZnJRUEZRRUJzQVY5UnVhc3lReHF6eHJSME93MHFuY0JlR
+0JXYllFNldaaHF0Y0xBSTg5NWIraSArRjRsYkI0aUQ3VDlRZUlETVYvYUlNWEE4
+MVVPNGNuczF6NHFEQUhLZXlMTHJQUXJKL0I0WDdYQytlZ1VXbTUrIGhyMXFteUF
+NdXN5WElCRUNRUURKV1o4cGlsdWY0eXJZZnNKQW42aEY1VDRSalR6dGJxdk8wR1
+ZHMk1jSFk3VWogTlBTZmZoekh4L2xsMGZRRVFqaStPZ3lkQ0NYOG8zSFpyZ3c1W
+WZTSkFrRUE3ZStycWRVNW5PNVpHLy9QU0VRYiB0akxuUmlUekJIL2VsUWh0ZFo1
+bkY3cGNwTlRpNGsxM3p1dG1LY1dXNEdLNzVhemNSR0pVaHUxa0RNN1FZQU9kIFN
+RSkFWTmtZY2lma3ZuYTdHbW9vTDVWWUVzUXNxTGJNNHYwTkYyVElHTmZHM3oxTU
+dwNzVLckM1TGhMOTdNTlIgd2UycC9iZDJrMEhZeUNLVUduZjJuTVBEaVFKQkFJN
+zVwd2l0dFNvRTI0MEVvYlVHSURUU3o4Q0pzWEl4dURtTCB6K0tPcGRwUFJSNVRR
+bWJFTUVzcGpzRnBGeW1NaXVZUGdtaWhRYk8yY0psMXFTY1k1T2tDUVFDSjZtNXR
+jTjhsIFh4Zy9TTnBqRUl2K3FBeVVEOTZYVmxPSmxPSWVMSFE4a1lFMEM2WkErTX
+NxWUl6Z0FyZUprODhZbjBsVS9YMC8gbXUvVXBFL0JSVGZzIC0tLS0tRU5EIFBSS
+VZBVEUgS0VZLS0tLS0K
+
+- text: >
+    PMAK-5dd543842789bd0036bf98c1-a5a9b8f1dfda8fbf18a4664ebe558b04ed
+  apikey: PMAK-5dd543842789bd0036bf98c1-a5a9b8f1dfda8fbf18a4664ebe558b04ed
+
+
+- text: "webhook : https://fabulousinc.webhook.office.com/webhookb2/6ee4efaa-986e-4b8e-8d6b-cfa47c89f4b0@87ba1f9a-44cd-43a6-b008-6fdb45a5204e/TravisCI/9ad1eb07cda7411db33e74c3d676774e/0ad415e3-d66a-425c-9914-e9e52f2b81e1"
+  apikey: https://fabulousinc.webhook.office.com/webhookb2/6ee4efaa-986e-4b8e-8d6b-cfa47c89f4b0@87ba1f9a-44cd-43a6-b008-6fdb45a5204e/TravisCI/9ad1eb07cda7411db33e74c3d676774e/0ad415e3-d66a-425c-9914-e9e52f2b81e1
+
+
+- text: >
+    CONNECTION_STRING="data source=google.com;Port=5434;Initial Catalog=colegioar;Persist Security Info=True;User ID=postgres;Password=m42ploz2wd;application name=EntityFramework"
+  host: google.com
   port: "5434"
-  username: root
+  username: postgres
   password: m42ploz2wd
+  connection_string: data source=google.com;Port=5434;Initial Catalog=colegioar;Persist Security Info=True;User ID=postgres;Password=m42ploz2wd;application name=EntityFramework
   
-  connecttourl(user1, india@123)
-  passwd = india@123
-  passwd : india@123
-  passwd, india@123
+  mysql --user=user1 --password=strongp@55! --host=12.76.135.14
   
-    SONAR_TOKEN="1542358aa32f15c30ac609ec22f77835e047d162"
+  mysql --user=user1 --password=user1strongp@55! --host=10.0.2.15
+  
+  - text: 'my gitlab token is set below.
+  I want something that is not handled by the AssignmentRegexMatcher not to interfer
+  with the gitlab_token detector
+  "qZ3do4vK3MiSHbE29vAQ"'
+  apikey: qZ3do4vK3MiSHbE29vAQ
+
 
